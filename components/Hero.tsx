46b01303272122ca1c10b741b4645dbf41c939be
Yasaman Karbasi <yasi_karbasi@yahoo.com>
2023-08-17 14:24:26 +0330

    feat: update tailwind

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,21 @@
 import React from 'react' 
-import Image from 'next/image'
+import {CustomButton} from './CustomButton'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          find, book, or rent a car -- quickly
+          and easily!
+        </h1>
+        <p className="hero__subtitle">
+          streamline your car rental experience with our effortless booking process.
+        </p>
+
+        <CustomButton />
+      </div>
+    </div>
   )
 }
 
